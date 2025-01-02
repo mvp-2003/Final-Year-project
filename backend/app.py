a63d6bf415c398d820fb80f626c3c2fdcc41a2b8
Rishabh <rishabh.babi@gmail.com>
2025-01-02 22:55:56 +0530

    Trial: Append DialoGPT messages + hardcoded questions

--- backend/app.py
+++ backend/app.py
@@ -34,32 +34,37 @@ def chat():
     current_details.update(new_details)
     chat_history[session_id]['details'] = current_details
     
+    dialog_history = chat_history[session_id]['dialog_history']
+    dialog_history.append(user_message)
+    
+    full_context = " ".join(dialog_history[-3:])
+    
+    input_ids = tokenizer.encode(full_context + tokenizer.eos_token, return_tensors='pt')
+    chat_response_ids = model.generate(
+        input_ids,
+        max_length=1000,
+        pad_token_id=tokenizer.eos_token_id,
+        temperature=0.7,
+        top_p=0.9,
+        no_repeat_ngram_size=3
+    )
+    
+    dialog_response = tokenizer.decode(chat_response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    
     structured_response = generate_response(current_details, user_message)
     
     if "I need some more information" in structured_response:
-        response = structured_response
+        if dialog_response and len(dialog_response.strip()) >= 10:
+            response = f"{dialog_response}\n\nBy the way, {structured_response.lower()}"
+        else:
+            response = structured_response
     else:
-        dialog_history = chat_history[session_id]['dialog_history']
-        dialog_history.append(user_message)
-        
-        full_context = " ".join(dialog_history[-3:])
-        
-        input_ids = tokenizer.encode(full_context + tokenizer.eos_token, return_tensors='pt')
-        chat_response_ids = model.generate(
-            input_ids,
-            max_length=1000,
-            pad_token_id=tokenizer.eos_token_id,
-            temperature=0.7,
-            top_p=0.9,
-            no_repeat_ngram_size=3
-        )
-        
-        response = tokenizer.decode(chat_response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-        
-        if not response or len(response.strip()) < 10:
+        if dialog_response and len(dialog_response.strip()) >= 10:
+            response = f"{dialog_response}\n\nAlso, {structured_response}"
+        else:
             response = structured_response
             
-        dialog_history.append(response)
-        chat_history[session_id]['dialog_history'] = dialog_history
+    dialog_history.append(response)
+    chat_history[session_id]['dialog_history'] = dialog_history
 
     return jsonify({'response': response})
