239ac6c9ccca66fda7e2d236c59a28fd2fb2482d
Rishabh <rishabh.babi@gmail.com>
2025-01-02 23:09:23 +0530

    Update

--- backend/app.py
+++ backend/app.py
@@ -32,46 +32,40 @@ def chat():
     if session_id not in chat_history:
         chat_history[session_id] = {
             'details': {},
-            'dialog_history': []
+            'dialog_history': [],
+            'context': "You are a helpful assistant collecting information about a suspect. Be friendly and engaging."
         }
-        
-    dialog_history = chat_history[session_id]['dialog_history']
-    
-    # For first message, use DialoGPT with a welcoming context
-    if not dialog_history:
-        welcome_context = "You are a helpful assistant collecting information about a suspect. Be friendly and engaging."
-        input_ids = tokenizer.encode(welcome_context + tokenizer.eos_token, return_tensors='pt')
-        chat_response_ids = model.generate(
-            input_ids,
-            max_length=1000,
-            pad_token_id=tokenizer.eos_token_id,
-            temperature=0.9,
-            top_p=0.9,
-            no_repeat_ngram_size=3
-        )
-        dialog_response = tokenizer.decode(chat_response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
-        
-        if not dialog_response or len(dialog_response.strip()) < 10:
-            from random import choice
-            dialog_response = choice(initial_prompts)
-    else:
-        dialog_history.append(user_message)
-        full_context = " ".join(dialog_history[-3:])
-        input_ids = tokenizer.encode(full_context + tokenizer.eos_token, return_tensors='pt')
-        chat_response_ids = model.generate(
-            input_ids,
-            max_length=1000,
-            pad_token_id=tokenizer.eos_token_id,
-            temperature=0.7,
-            top_p=0.9,
-            no_repeat_ngram_size=3
-        )
-        dialog_response = tokenizer.decode(chat_response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
     
     new_details = extract_details(user_message)
     current_details = chat_history[session_id]['details']
     current_details.update(new_details)
     chat_history[session_id]['details'] = current_details
+    
+    dialog_history = chat_history[session_id]['dialog_history']
+    context = chat_history[session_id]['context']
+    
+    if not dialog_history:
+        input_text = context
+    else:
+        dialog_history.append(user_message)
+        recent_messages = dialog_history[-3:] if len(dialog_history) > 3 else dialog_history
+        input_text = context + " " + " ".join(recent_messages)
+    
+    input_ids = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors='pt')
+    chat_response_ids = model.generate(
+        input_ids,
+        max_length=1000,
+        pad_token_id=tokenizer.eos_token_id,
+        temperature=0.7,
+        top_p=0.9,
+        no_repeat_ngram_size=3
+    )
+    
+    dialog_response = tokenizer.decode(chat_response_ids[:, input_ids.shape[-1]:][0], skip_special_tokens=True)
+    
+    if not dialog_history and (not dialog_response or len(dialog_response.strip()) < 10):
+        from random import choice
+        dialog_response = choice(initial_prompts)
     
     structured_response = generate_response(current_details, user_message)
     
@@ -85,8 +79,23 @@ def chat():
             response = f"{dialog_response}\n\nAlso, {structured_response}"
         else:
             response = structured_response
-            
+    
     dialog_history.append(response)
     chat_history[session_id]['dialog_history'] = dialog_history
+    
+    detail_context = []
+    if current_details:
+        if 'gender' in current_details:
+            detail_context.append(f"The suspect is {current_details['gender']}")
+        if 'hair color' in current_details:
+            detail_context.append(f"has {current_details['hair color']} hair")
+        if 'face shape' in current_details:
+            detail_context.append(f"has a {current_details['face shape']} face")
+        if 'has moustache' in current_details:
+            has_moustache = "has" if current_details['has moustache'].lower() == 'yes' else "doesn't have"
+            detail_context.append(f"{has_moustache} a moustache")
+    
+    if detail_context:
+        chat_history[session_id]['context'] = context + " " + ", ".join(detail_context) + "."
 
     return jsonify({'response': response})
