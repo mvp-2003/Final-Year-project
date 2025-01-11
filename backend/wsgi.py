24d3292f6ae862011c458a33dfe793e432a53320
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2025-01-11 17:04:53 +0530

    Update wsgi.py for using pyngrok

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,7 +1,9 @@
 from app import app
-from flask_ngrok import run_with_ngrok
+from pyngrok import ngrok
 
-run_with_ngrok(app)
+ngrok.set_auth_token("YOUR_NGROK_AUTH_TOKEN")
+public_url = ngrok.connect(5000)
+print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:5000\"".format(public_url))
 
 if __name__ == "__main__":
     app.run()
